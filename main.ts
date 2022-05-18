8e820efd9475954e81b87bab3b8a7fe4cd2abcd8
aha ha Crabronidae <ahawespe@gmail.com>
2022-05-18 13:55:45 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
+let sprite = game.createSprite(2, 2)
 basic.forever(function () {
-	
+    if (sprite.isTouchingEdge()) {
+        sprite.ifOnEdgeBounce()
+    }
+    if (serial.readLine() == "w") {
+        sprite.change(LedSpriteProperty.Y, 1)
+    } else if (serial.readLine() == "a") {
+        sprite.change(LedSpriteProperty.X, -1)
+    } else if (serial.readLine() == "s") {
+        sprite.change(LedSpriteProperty.Y, -1)
+    } else if (serial.readLine() == "d") {
+        sprite.change(LedSpriteProperty.X, 1)
+    } else {
+        serial.writeLine("?")
+    }
 })
